483e52ee52bb107dced720a56db85dfe8c66a0de
Richard Rodriguez <richard.rodriguez@qcode.co>
2023-12-31 00:47:11 -0500

    midu class components if for

--- ricky-app/src/app/app.component.ts
+++ ricky-app/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ricky-app';
+  city = 'barcelona'
 }
---
+++ ricky-app/src/app/user/user.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [
+    NgForOf,
+    NgIf
+  ],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  games = [
+    {
+      id: 1,
+      name: 'League of Legends',
+      platform: 'PC',
+    },
+    {
+      id: 2,
+      name: 'Dota 2',
+      platform: 'PC',
+    },
+    {
+      id: 3,
+      name: 'Counter Strike',
+      platform: 'PC',
+    },
+    {
+      id: 4,
+      name: 'Bloodborne',
+      platform: 'Console',
+    }
+  ];
+  isPcPlatform (platform: string) {
+     return platform.toLowerCase() === "pc" ? true : false
+  }
+  estoEsUnaPrueba () {
+  console.log('esto es una prueba')
+  return 'esto es una prueba'
+
+}
+
+}
+
